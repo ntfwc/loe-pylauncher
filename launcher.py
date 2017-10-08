bc6491525a2324694d465bf1228b4790d12ca102
ntfwc <ntfwc@yahoo.com>
2017-10-08 00:44:13 -0500

    Moved game launching to main program
    The launcher should just get the game path from the dialog, so there
    shouldn't be a need for another module.

--- launcher.py
+++ launcher.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-
 from lib.dialog import startDialog
-from lib.game_launch import launchGame
+from subprocess import Popen
+
+GAME_PATH="/usr/bin/leafpad"
 
 def main():
     userSelectedLaunch = startDialog()
     if userSelectedLaunch:
-        launchGame()
+        launchGame(GAME_PATH)
 
+def launchGame(gamePath):
+    print("Launching game...")
+    Popen([gamePath])
 
 if __name__ == "__main__":
     main()
--- lib/game_launch.py
+++
@@ -1,7 +0,0 @@
-from subprocess import Popen
-
-GAME_PATH="/usr/bin/leafpad"
-
-def launchGame():
-    print("Launching game!")
-    Popen([GAME_PATH])
