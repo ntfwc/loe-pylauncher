96273e6b1c03882134235b674708cd918da06c4e
ntfwc <ntfwc@yahoo.com>
2017-10-08 21:16:46 -0500

    Changed launcher to save the selected game directory

--- launcher.py
+++ launcher.py
@@ -3,18 +3,25 @@ import lib.dialog
 import lib.game_paths
 
 from subprocess import Popen
-from os.path import exists
+import os.path
+import sys
+
 
 TITLE="Launcher"
 GAME_DIR_DIALOG_TITLE="Please select the game directory"
 INVALID_GAME_DIR_TITLE="Invalid game directory"
 INVALID_GAME_DIR_RETRY_MESSAGE="The game directory does not look valid. Select a different directory?"
 
+GAME_DIR_CFG_FILE="game_dir.cfg"
 GAME_PATH="/usr/bin/leafpad"
 
-def main():
-    lib.dialog.init()
+script_dir = None
 
+def main():
+    global script_dir
+    script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+    lib.dialog.init()
     gameDirectory = getGameDirectory()
     if gameDirectory == None:
         return
@@ -26,17 +33,33 @@ def main():
         launchGame(GAME_PATH)
 
 def getGameDirectory():
+    gameDirCfgPath = os.path.join(script_dir, GAME_DIR_CFG_FILE)
+    if os.path.exists(gameDirCfgPath):
+        gameDirectory = readFile(gameDirCfgPath)
+        if validateGameDirectory(gameDirectory):
+            return gameDirectory
+        print("Configured game directory '%s' is not valid" % gameDirectory)
+
     while True:
         gameDirectory = lib.dialog.askUserForDirectory(GAME_DIR_DIALOG_TITLE)
         if gameDirectory == ():
             return None
         if validateGameDirectory(gameDirectory):
+            writeFile(gameDirCfgPath, gameDirectory)
             return gameDirectory
         if not lib.dialog.askYesOrNo(INVALID_GAME_DIR_TITLE,INVALID_GAME_DIR_RETRY_MESSAGE):
             return None
 
 def validateGameDirectory(gameDirectory):
-    return exists(lib.game_paths.getResourcesPath(gameDirectory))
+    return os.path.exists(lib.game_paths.getResourcesPath(gameDirectory))
+
+def readFile(filePath):
+    with open(filePath, "r") as f:
+        return f.read()
+
+def writeFile(filePath, text):
+    with open(filePath, "w") as f:
+        return f.write(text)
 
 def launchGame(gamePath):
     print("Launching game...")
