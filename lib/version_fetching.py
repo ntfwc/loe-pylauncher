1fc0f05d7f43aea82937a55599547cb8cc374e73
ntfwc <ntfwc@yahoo.com>
2017-10-12 21:47:44 -0500

    Added method for available version to version_fetching

--- lib/version_fetching.py
+++ lib/version_fetching.py
@@ -1,5 +1,6 @@
 from lib.game_paths import getResourcesPath
 from lib.game_data_parsing import extractBuildManifest,parseBuildManifest
+import lib.loe_api
 
 import sys
 
@@ -12,3 +13,15 @@ def getInstalledVersionId(gameDirectory):
     except:
         print("Failed to get installed version ID:", sys.exc_info()[1])
         return None
+
+def fetchAvailableVersionId():
+    try:
+        availableVersion = lib.loe_api.parseLatestVersion(lib.loe_api.downloadVersionsJson())
+        if availableVersion == None:
+            print("Failed to parse available version")
+        return availableVersion
+    except:
+        print("Failed to fetch available version")
+        return None
+
+
